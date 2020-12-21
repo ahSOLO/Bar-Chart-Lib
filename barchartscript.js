b4c32683f1489ef28df41ec237e4dd4469a094c0
Andrew Han <junmenghan@gmail.com>
2020-12-21 04:21:40 +0000

    refactored creation of bars and setting of width into functions

--- barchartscript.js
+++ barchartscript.js
@@ -12,28 +12,32 @@ let axisLabelWidth = 25;
 let barWidth = chartWidth-axisLabelWidth;
 
 // Initialization
-$("#chart").width(chartWidth);
-$("#axis-labels").width(axisLabelWidth);
-$("#bars").width(barWidth);
-
-let index = 0;
-$.each(data,function(property, value){
-  $("#bars").append('<li><div class="bar"><span class=bar-value>'+value+'</span></div><span class=bar-title>'+property+'</span></li>')
-  $(".bar").eq(index)
-  .height(300 * value/total)
-  .css("margin-left", (barWidth * 0.15 / length)+'px' )
-  ;
-  index++;
-});
-
-$("#bars li").width(barWidth / length);
+createBars();
+setWidth();
 
 // Adjust width and margins of bars on window resize
 $(window).resize(function() {
   chartWidth = Math.max(300, Math.min($(window).width()-100, 1000));
   barWidth = chartWidth-axisLabelWidth;
+  setWidth();
+});
+
+// Set width and margins of chart and bars
+function setWidth(){
   $("#chart").width(chartWidth);
   $("#axis-labels").width(axisLabelWidth);
   $("#bars").width(barWidth);
+  $("#bars li").width(barWidth / length);
   $(".bar").css("margin-left", barWidth * 0.15 / length);
-});
+  }
+
+function createBars(){
+  let index = 0;
+  $.each(data,function(property, value){
+    $("#bars").append('<li><div class="bar"><span class=bar-value>'+value+'</span></div><span class=bar-title>'+property+'</span></li>')
+    $(".bar").eq(index)
+    .height(300 * value/total)
+    ;
+    index++;
+  });
+  }
