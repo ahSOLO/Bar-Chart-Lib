2944cdb259d99f2035aea1d554dc0fd833cf46da
Andrew Han <junmenghan@gmail.com>
2020-12-21 23:45:17 +0000

    completed feature: customizable bar spacing

--- barchartscript.js
+++ barchartscript.js
@@ -1,6 +1,6 @@
 // Input Test Variable
 drawBarChart({First: 50, Second: 30, Third: 20},
-  {valuePosition: "top", barColors: ["#000000", "#C0C0C0", "#008080"], labelColor: "#FF0000"},
+  {defaultWidth: 1000, valuePosition: "top", barColors: ["#000000", "#C0C0C0", "#008080"], labelColor: "#FF0000", barWidth: "60%"},
   $("body"));
 
 function drawBarChart(data, options, element){
@@ -19,32 +19,25 @@ function drawBarChart(data, options, element){
   // Width + Height Variables
   let chartWidth = defaultWidth;
   let axisLabelWidth = 25;
-  let barWidth = chartWidth-axisLabelWidth;
+  let BarsTotalWidth = chartWidth-axisLabelWidth;
   let barHeight = 300;
 
   // Initialization
   createChart();
   createBars();
   setWidth();
+  if (options.autoWidth !== "off") autoWidth();
   if (options.valuePosition) setValuePos(options.valuePosition);
   if (options.barColors) setBarColors(options.barColors);
   if (options.labelColor) setLabelColor(options.labelColor);
-
-  // Adjust width and margins of bars on window resize
-  if (!options.chartWidth) {
-    $(window).resize(function() {
-      chartWidth = Math.max(300, Math.min($(window).width()-100, defaultWidth)); // Clamp the value of chartwidth to between 300 and window width - 100
-      barWidth = chartWidth-axisLabelWidth;
-      setWidth();
-    });
-  }
+  if (options.barWidth) setBarSpacing(options.barWidth);
 
   // Lay down HTML for chart axes and title
   function createChart(){
     element.prepend('<div id="bar-chart-lib"><div id="title"><p>Bar Chart Title</p></div><div id="chart"><ul id="axis-labels"><li><span>100%</span></li><li><span>75%</span></li><li><span>50%</span></li><li><span>25%</span></li></ul><ul id="bars"></ul></div></div>')
   }
 
-  // Create bars and assign horizontal value based on % of total data value
+  // Create bars and assign height based on % of total data value
   function createBars(){
     let index = 0;
     $.each(data,function(property, value){
@@ -70,9 +63,18 @@ function drawBarChart(data, options, element){
   function setWidth(){
     $("#chart").width(chartWidth);
     $("#axis-labels").width(axisLabelWidth);
-    $("#bars").width(barWidth);
-    $("#bars li").width(barWidth / length);
-    $(".bar").css("margin-left", barWidth * 0.15 / length);
+    $("#bars").width(BarsTotalWidth);
+    $("#bars li").width(BarsTotalWidth / length);
+    $(".bar").css("margin-left", BarsTotalWidth * 0.15 / length);
+  }
+
+  // Adjust width and margins of bars on window resize
+  function autoWidth(){
+    $(window).resize(function() {
+      chartWidth = Math.max(300, Math.min($(window).width()-100, defaultWidth)); // Clamp the value of chartwidth to between 300 and window width - 100
+      BarsTotalWidth = chartWidth-axisLabelWidth;
+      setWidth();
+    });
   }
 
   // Set color of bars to a single color or individually according to an array of colors
@@ -81,6 +83,11 @@ function drawBarChart(data, options, element){
     else if (Array.isArray(color)) $.each(color, function(index) {
       $(".bar").eq(index).css("background-color", color[index]);
     });
+  }
+
+  // Set width of bars and space between bars
+  function setBarSpacing(widthPercent){
+    $(".bar").width(widthPercent).css("margin-left", BarsTotalWidth * ((1 - parseFloat(widthPercent)/100)/2) / length )
   }
 
   // Set color of label text
